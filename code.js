59d52aea4457367813b0db386e51a4e532335304
Zubair Nazir <Zubairwani49@gmail.com>
2024-03-11 12:58:26 +0530

    changed border radius for clock

--- code.js
+++ code.js
@@ -2,7 +2,12 @@ const clock = document.getElementById('clock');
 // const clock = document.querySelector('#clock')
 
 setInterval(function () {
+  
+  
+  
   let date = new Date();
+  
+  
   // console.log(date.toLocaleTimeString());
   clock.innerHTML = date.toLocaleTimeString();
 }, 1000);
